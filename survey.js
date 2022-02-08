bff4240180da1c1e3770602718d89b5eb11db529
ConsensusAI <malik.iqbal@hotmail.ca>
2022-02-08 02:18:52 +0000

    Lint code

--- survey.js
+++ survey.js
@@ -13,7 +13,7 @@ rl.question('What\'s your name? Nicknames are also acceptable :) ', (name) => {
         rl.question('What\'s your favourite thing to eat for that meal? ', (food) => {
           rl.question('Which sport is your absolute favourite? ', (sport) => {
             rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (superpower) => {
-              console.log(`Hi, my name is ${name}! I like ${activity} while listening to ${music}. I love eating ${food} for ${meal}. My absolute favourite sport is ${sport}. My special superpower is ${superpower}!`)
+              console.log(`Hi, my name is ${name}! I like ${activity} while listening to ${music}. I love eating ${food} for ${meal}. My absolute favourite sport is ${sport}. My special superpower is ${superpower}!`);
               rl.close();
             });
           });
